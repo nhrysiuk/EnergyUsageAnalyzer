bddf639948f6dcb12597db3292c6665b9e99cdde
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-05-14 21:13:58 +0300

    add location accuracy visitor

---
+++ Sources/Location/Accuracy/LocationAccuracyManager.swift
@@ -0,0 +1,27 @@
+import SwiftSyntax
+import CoreLocation
+
+class LocationAccuracyManager: EnergyVisitable {
+    private var views: [String] = []
+    
+    let inefficientConfigs: [(String, CLLocationDistance)] = [("kCLLocationAccuracyBest", 256),("kCLLocationAccuracyBestForNavigation", 256),("kCLLocationAccuracyBest", 16)]
+    
+    func analyze(_ sourceFile: SourceFileSyntax) {
+        let startVisitor = LocationAccuracyVisitor(viewMode: .sourceAccurate)
+        startVisitor.walk(sourceFile)
+        
+        let config = startVisitor.getConfig()
+        if inefficientConfigs.contains (where: { x in
+            x == config
+        }) {
+            print("Found inefficient location accuracy configuration:")
+            print("- Accuracy Level: \(config.accuracyLevel)")
+            print("- Distance Filter: \(config.distance)")
+            views = startVisitor.getViews()
+        }
+    }
+    
+    func getViews() -> [String] {
+        return views
+    }
+}
---
+++ Sources/Location/Accuracy/LocationAccuracyVisitor.swift
@@ -0,0 +1,72 @@
+import SwiftSyntax
+import CoreLocation
+
+class LocationAccuracyVisitor: SyntaxVisitor, EnergyVisitable {
+    
+    private enum AccuracyProperty: String {
+        case desiredAccuracy
+        case distanceFilter
+        
+        func processValue(_ node: ExprSyntax) -> (String?, Double?) {
+            switch self {
+            case .desiredAccuracy:
+                if let word = node.as(DeclReferenceExprSyntax.self) {
+                    return (word.baseName.text, nil)
+                }
+            case .distanceFilter:
+                if let intLiteral = node.as(IntegerLiteralExprSyntax.self) {
+                    return (nil, Double(intLiteral.literal.text))
+                }
+                if let doubleLiteral = node.as(FloatLiteralExprSyntax.self) {
+                    return (nil, Double(doubleLiteral.literal.text))
+                }
+            }
+            return (nil, nil)
+        }
+    }
+    
+    private struct AccuracyConfig {
+        var accuracyLevel: String
+        var distance: CLLocationDistance
+        
+        static let `default` = AccuracyConfig(
+            accuracyLevel: "kCLLocationAccuracyBest",
+            distance: 16
+        )
+    }
+    
+    private var views: [String] = []
+    private var config: AccuracyConfig = .default
+    
+    func analyze(_ sourceFile: SourceFileSyntax) {
+        walk(sourceFile)
+    }
+    
+    override func visit(_ node: SequenceExprSyntax) -> SyntaxVisitorContinueKind {
+        guard let memberAccess = node.elements.first?.as(MemberAccessExprSyntax.self),
+              let property = AccuracyProperty(rawValue: memberAccess.declName.baseName.text),
+              let lastElement = node.elements.last else {
+            return .visitChildren
+        }
+        
+        let (accuracyValue, distanceValue) = property.processValue(lastElement)
+        
+        if let accuracyValue = accuracyValue {
+            config.accuracyLevel = accuracyValue
+            views.append(node.description)
+        } else if let distanceValue = distanceValue {
+            config.distance = distanceValue
+            views.append(node.description)
+        }
+        
+        return .visitChildren
+    }
+    
+    func getViews() -> [String] {
+        return views
+    }
+    
+    func getConfig() -> (accuracyLevel: String, distance: CLLocationDistance) {
+        return (config.accuracyLevel, config.distance)
+    }
+}
