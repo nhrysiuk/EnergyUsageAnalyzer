75ccd579dd3fffc4f8a927dc90a9be418b7ee6a7
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-05-15 18:08:20 +0300

    add draw allocation visitor

---
+++ Sources/CPU/DrawAllocationVisitor.swift
@@ -0,0 +1,34 @@
+import Foundation
+import SwiftSyntax
+
+class DrawAllocationVisitor: SyntaxVisitor, EnergyVisitable {
+    
+    func analyze(_ sourceFile: SwiftSyntax.SourceFileSyntax) {
+        walk(sourceFile)
+    }
+    
+    private var views: [String] = []
+    
+    override func visit(_ node: FunctionDeclSyntax) -> SyntaxVisitorContinueKind {
+        if node.modifiers.contains(where: { $0.name.text == "override" }),
+           node.name.text == "draw",
+           let parameters = node.signature.parameterClause.parameters.first,
+           parameters.type.as(IdentifierTypeSyntax.self)?.name.text == "CGRect",
+           let statements = node.body?.statements {
+            checkEachFunc(for: statements)
+        }
+        return .visitChildren
+    }
+    
+    private func checkEachFunc(for statements: CodeBlockItemListSyntax) {
+        for statement in statements {
+            if let assignment = statement.item.as(VariableDeclSyntax.self) {
+                views.append(statement.description.trimmingCharacters(in: .whitespacesAndNewlines))
+            }
+        }
+    }
+    
+    func getViews() -> [String] {
+        return views
+    }
+}
