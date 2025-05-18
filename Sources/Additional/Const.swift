7497386f1ee64aabdf0962574890b4bdeea848f4
Anastasiia Hrysiuk <ngrysuk946@gmail.com>
2025-05-18 16:56:49 +0300

    fix array name

--- Sources/Additional/Const.swift
+++ Sources/Additional/Const.swift
@@ -1,5 +1,5 @@
 import Foundation
 
 struct Const {
-    let visitors: [any EnergyVisitable] = [BluetoothManager(), InlineMethodManager(), InstanceFunctionsManager(), DrawAllocationManager(), FinalClassManager(), ParameterObjectManager(), OpacityManager(), BlurManager(), ShadowManager(), LocationStopManager(), LocationAccuracyManager(), PublishTimerManager(), ScheduledTimerManager(), ToleranceTimerManager()]
+    let allVisitors: [EnergyVisitable] = [BluetoothManager(), InlineMethodManager(), InstanceFunctionsManager(), DrawAllocationManager(), FinalClassManager(), ParameterObjectManager(), OpacityManager(), BlurManager(), ShadowManager(), LocationStopManager(), LocationAccuracyManager(), PublishTimerManager(), ScheduledTimerManager(), ToleranceTimerManager()]
 }
